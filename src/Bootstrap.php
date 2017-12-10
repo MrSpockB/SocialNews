5932c5c14a5b96991a0ee76a9b5eb52896b9b5e9
Jose Francisco Peña Estrada <francisco@beluga.solutions>
2017-12-10 11:45:29 -0600

    added twigFactory and pass an instance when creating the controller

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -41,7 +41,9 @@ switch ($routeInfo[0]) {
         [$controllerName, $method] = explode('#', $routeInfo[1]);
         $vars = $routeInfo[2];
 
-        $controller = new $controllerName;
+        $factory = new SocialNews\Framework\Rendering\TwigTemplateRendererFactory();
+        $templateRenderer = $factory->create();
+        $controller = new $controllerName($templateRenderer);
         $response = $controller->$method($request, $vars);
         break;
 }
